37114539f0b06e1f01131104a38b7bc4bf4a9252
tranbao12322 <trangiabao1122tp@gmail.com>
2025-09-20 21:11:05 +0700

    chat

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';  // Chỉ cần 1 lần import này
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
